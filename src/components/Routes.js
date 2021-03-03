651abbc54efd193d31785e303fc5af2834d9df4f
hritikaggarwal2 <hritikaggarwal2@gmail.com>
2021-03-02 23:49:15 -0800

    updated nav

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // user provider
@@ -16,8 +15,7 @@ import Login from "../screens/Login";
 import Loading from "../screens/Loading";
 
 // components
-import NavBar from "./NavBar";
-import HeaderBar from "./HeaderBar";
+import PageWithNav from "../components/PageWithNav";
 
 function Routes() {
   const user = useUser().user;
@@ -29,17 +27,21 @@ function Routes() {
         <Loading />
       ) : user ? (
         <Router>
-          <NavBar />
-          <HeaderBar />
           <Switch>
             <Route path="/employees">
-              <Employees />
+              <PageWithNav index={2}>
+                <Employees />
+              </PageWithNav>
             </Route>
             <Route path="/payroll">
-              <Payroll />
+              <PageWithNav index={1}>
+                <Payroll />
+              </PageWithNav>
             </Route>
             <Route path="/documents">
-              <Documents />
+              <PageWithNav index={3}>
+                <Payroll />
+              </PageWithNav>
             </Route>
             <Route path="/profile">
               <EmployeeDetail />
@@ -47,7 +49,11 @@ function Routes() {
             <Route path="/addemployee">
               <AddEmployee />
             </Route>
-            <Route path="*" component={Dashboard} />
+            <Route path="*">
+              <PageWithNav index={0}>
+                <Dashboard />
+              </PageWithNav>
+            </Route>
           </Switch>
         </Router>
       ) : (
