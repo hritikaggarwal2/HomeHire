4c614bdbb5698075b18b5e76810f36b70cfa9b03
Esther Chien <estchien@cs.washington.edu>
2021-03-09 19:16:06 -0800

    Slide12 styling (#35)
    * change to 130 size img
    
    * margin one liner
    
    * 130 130
    
    * change margin
    
    * styling

--- src/screens/EmployeeDetail.js
+++ src/screens/EmployeeDetail.js
@@ -84,10 +84,10 @@ export default function EmployeeDetail(props) {
         <div className={(props.uid ? "pageWithNav " : "") + "userProfile"}>
           <div>
             <div className="sqContainer">
-              <img src={image} alt="user profile" />
+              <img src={image} alt="user profile"/>
             </div>
             <div>
-              <h1>{data.full_name} </h1>
+              <h2>{data.full_name} </h2>
 
               {/* <Row>
                 <Col>
@@ -123,7 +123,7 @@ export default function EmployeeDetail(props) {
 
               <List>
                 <ListItem>
-                  <ListItemIcon>
+                  <ListItemIcon className="pest">
                     <DesktopMacIcon />
                   </ListItemIcon>
                   <ListItemText primary={data.role} />
